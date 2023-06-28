393db52847daa9e3da21e0893dbba3a80cd07585
Diaksey <troplourede@gmail.com>
2023-06-28 10:59:24 +0200

    Instanciation de récupération de chemin

---
+++ JS/router.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Variable de configuration des routes
+ */
+const routerConfif = {
+  routes: [],
+};
+
+/**
+ *
+ * Fonction de maintient la route
+ *
+ */
+export const handleRoute = () => {
+  const pathName = location.pathname;
+  console.log(pathName);
+};
