a018ed701486b400982ceccccf9600b45efba76d
Diaksey <troplourede@gmail.com>
2023-06-28 13:55:28 +0200

    Initialisation des pages dans un autre dossier

---
+++ JS/js-views/home.js
@@ -0,0 +1,5 @@
+export const initHome= () => {
+    document.querySelector("#home button").addEventListener("click", () => {
+      alert("Pranked");
+    });
+}
--- JS/router.js
+++ JS/router.js
@@ -1,41 +1,86 @@
+import { initHome } from "./js-views/home.js";
+
 /**
  * Variable de configuration des routes
  */
-const routerConfif = {
+const routeConfig = {
   routes: [
     {
-        path:'/thumbnail',
-        initialisation:undefined,
-        templateUrl:'/view/thumbnail.html'
+      path: "/thumbnail",
+      initialisation: undefined,
+      templateUrl: "/view/thumbnail.html",
     },
     {
-        path:'/',
-        initialisation:undefined,
-        templateUrl:'/view/home.html'
+      path: "/",
+      initialisation:initHome,
+      templateUrl: "/view/home.html",
     },
     {
-        path:'/break',
-        initialisation:undefined,
-        templateUrl:'/view/CeCheminNexistePas.html'
-    }
-    
+      path: "/break",
+      initialisation: undefined,
+      templateUrl: "/view/CeCheminNexistePas.html",
+    },
+    {
+      path: "/editor",
+      initialisation: undefined,
+      templateUrl: "/view/editor.html",
+    },
+    {
+      path: "/azertyuiop",
+      initialisation: undefined,
+      templateUrl: "/view/azertyuiop.html",
+    },
   ],
 };
 
-class Router{
-    /**
-     * Manage la route en cours
-     */
-     handleRoute(){
-        const pathName = location.pathname;
-        console.log(pathName);
-     }
-     /**
-      * Allez à
-      * @param {string} pathName Chemin commencant par / 
-      */
-     changeRoute(pathName){}
+class Router {
+  #currentRoute;
+  get currentRoute() {
+    return this.#currentRoute;
+  }
+  // set currentRoute(value){this.#currentRoute=value}
+  /**
+   * Manage la route en cours
+   */
+  handleRoute() {
+    const pathName = location.pathname;
+    console.log(pathName);
+    this.#currentRoute = routeConfig.routes.find(
+      (route) => route.path == pathName
+    );
+    this.#instanciateCurrentRouteTemplate();
+  }
+  /**
+   * Allez à
+   * @param {string} pathName Chemin commencant par /
+   */
+  changeRoute(pathName) {}
+  /**
+   * Initialise le contenu du template si non présent
+   * et déclenche le chargement DOM du contenu
+   */
+  #instanciateCurrentRouteTemplate() {
+    if (undefined !== this.#currentRoute.templateText) {
+      this.#loadCurrentDOMContent();
+    } else {
+      fetch(this.#currentRoute.templateUrl)
+        .then((resp) => resp.text())
+        .then((t) => {
+          this.#currentRoute.templateText = t;
+          this.#loadCurrentDOMContent();
+        });
+    }
+  }
+  /**
+   * Chargement du contenu text/html de la vue dans le noeud du selecteur
+   * @param {string} domContainerSelector css selecteur du noeud a charger pour la
+   */
+  #loadCurrentDOMContent(domContainerSelector = "article") {
+    document.querySelector(domContainerSelector).innerHTML =
+      this.#currentRoute.templateText;
+    if (undefined !== this.#currentRoute.initialisation) {
+      this.#currentRoute.initialisation();
+    }
+  }
 }
-const router = new Router();
-router.handleRoute();
-router.changeRoute();
+export const router = new Router();
